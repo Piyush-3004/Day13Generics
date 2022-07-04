5cf5419e1e3a28b4c133f72a9dceccfbe1cf0691
Chetan Patil <piyushp0541@gmail.com>
2022-07-05 01:18:18 +0530

    extend max method totake morethan three parameters

--- src/main/java/com/bz/mvnp/Generics/App.java
+++ src/main/java/com/bz/mvnp/Generics/App.java
@@ -3,15 +3,18 @@ package com.bz.mvnp.Generics;
 public class App {
 	
 	
-	public <T extends Comparable<T>> T findMax(T element1,T element2,T element3){
-		if(element1.compareTo(element2) >0 && element1.compareTo(element3)>0) 
+	public <T extends Comparable<T>> T findMax(T element1, T element2, T element3, T element4) {
+		if (element1.compareTo(element2) > 0 && element1.compareTo(element3) > 0 && element1.compareTo(element4) > 0)
 			return element1;
-			else if(element2.compareTo(element1) >0 && element2.compareTo(element3)>0)
+		else if (element2.compareTo(element1) > 0 && element2.compareTo(element3) > 0
+				&& element2.compareTo(element4) > 0)
 			return element2;
-			else 
-				return element3;
+		else if (element3.compareTo(element1) > 0 && element3.compareTo(element2) > 0
+				&& element3.compareTo(element4) > 0)
+			return element3;
+		else
+			return element4;
 	}
-	
 
 
 }
--- src/test/java/com/bz/mvnp/Generics/AppTest.java
+++ src/test/java/com/bz/mvnp/Generics/AppTest.java
@@ -4,20 +4,12 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 public class AppTest {
-	
+
 	Integer num1 = new Integer(10);
 	Integer num2 = new Integer(20);
 	Integer num3 = new Integer(30);
-
-	Float floatNum1 = new Float(100.20);
-	Float floatNum2 = new Float(20.0);
-	Float floatNum3 = new Float(10.10);
-
-	String str1 = "Apple";
-	String str2 = "Peach";
-	String str3 = "Banana";
+	Integer num4 = new Integer(40);
 
 	static App test;
 
@@ -26,70 +18,12 @@ public class AppTest {
 
 		test = new App();
 	}
-	
-	@Test 
+
+	@Test
 	public void findMaxTest() {
-		Integer actual = test.findMax(num1,num2,num3);
-		Integer result = 30;
-		Assert.assertEquals(result, actual);
-	}
-	
-	public void maxTestmiddle() {
-		int actual = test.findMax(num1, num3, num2);
-		int result = 30;
+		Integer actual = test.findMax(num1, num2, num3, num4);
+		Integer result = 100;
 		Assert.assertEquals(result, actual);
 	}
 
-	@Test
-	public void maxTestAtThird() {
-		int actual = test.findMax(num1, num2, num3);
-		int result = 30;
-		Assert.assertEquals(result, actual);
-	}
-
-	@Test
-	public void maxFloatTest() {
-		float actual = test.findMax(floatNum1, floatNum2, floatNum3);
-		float result = 100.20f;
-		Assert.assertEquals(result, actual, 0.0);
-	}
-
-	@Test
-	public void maxFloatTestMiddle() {
-		float actual = test.findMax(floatNum2, floatNum1, floatNum3);
-		float result = 100.20f;
-		Assert.assertEquals(result, actual, 0.0);
-
-	}
-
-	@Test
-	public void maxFloatTestAtLast() {
-		float actual = test.findMax(floatNum2, floatNum3, floatNum1);
-		float result = 100.20f;
-		Assert.assertEquals(result, actual, 0.0);
-
-	}
-
-	@Test
-	public void maxString() {
-		String actual = test.findMax(str3, str2, str1);
-		String result = "Banana";
-		Assert.assertEquals(result, actual);
-
-	}
-
-	@Test
-	public void maxStringMidddle() {
-		String actual = test.findMax(str1, str3, str2);
-		String result = "Banana";
-		Assert.assertEquals(result, actual);
-	}
-
-	@Test
-	public void maxStringAtLast() {
-		String actual = test.findMax(str1, str2, str3);
-		String result = "Banana";
-		Assert.assertEquals(result, actual);
-
-	}
 }
